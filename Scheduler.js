71b9b23b0b0d505440ce12e7127cbdbd710740c6
Peter2021s <92271070+Peter2021s@users.noreply.github.com>
2021-10-15 19:15:06 -0400

    Update Scheduler.js

--- Scheduler.js
+++ Scheduler.js
@@ -1,18 +1,16 @@
 //class Scheduler{
 
-
-  //  Scheduleprocess(processes, n, sl, wt, quantum) 
-     //{
-    // }
+//Scheduleprocess(processes, n, sl, wt, quantum)    
+//{
+// }
  
-//    }    
-  //  let processes = [1, 2, 3,4];
-    //var n = processes.length;
-       
-    //var Schedule= 1/n;
-     // Time quantum
- //   quantum = 12;
-   // Scheduleprocess(processes, n, Schedule, quantum)
+//}    
+//let processes = [1, 2, 3,4];
+//var n = processes.length;
+//var Schedule= 1/n;
+// Time quantum
+// quantum = 12;
+// Scheduleprocess(processes, n, Schedule, quantum)
 
     
     
